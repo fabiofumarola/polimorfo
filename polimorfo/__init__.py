11aa0c3736efd4fd506b300ea66d14e7b3fdd7c4
Fabio Fumarola <fumarolaf@prometeia.com>
2020-10-28 18:19:25 +0100

    Bump version: 0.9.6 → 0.9.7

--- polimorfo/__init__.py
+++ polimorfo/__init__.py
@@ -2,5 +2,5 @@
 
 __author__ = """Fabio Fumarola"""
 __email__ = 'fabiofumarola@gmail.com'
-__version__ = '0.9.6'
+__version__ = '0.9.7'
 __cache_dir__ = '~/.polimorfo'
--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fabiofumarola/polimorfo',
-    version='0.9.6',
+    version='0.9.7',
     zip_safe=False,
 )
