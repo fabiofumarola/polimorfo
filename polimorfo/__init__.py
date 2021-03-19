122ab7bf01befbf5cc0ecd10e2504fd540e31ca5
Fabio Fumarola <fumarolaf@prometeia.com>
2021-03-19 11:46:43 +0100

    Bump version: 0.9.60 → 0.9.61

--- polimorfo/__init__.py
+++ polimorfo/__init__.py
@@ -2,5 +2,5 @@
 
 __author__ = """Fabio Fumarola"""
 __email__ = "fabiofumarola@gmail.com"
-__version__ = "__version__ = '0.9.60'"
+__version__ = "__version__ = '0.9.61'"
 __cache_dir__ = "~/.polimorfo"
