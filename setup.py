52737570a4fe777d11e71b70f202eb0d49064dab
Fabio Fumarola <fumarolaf@prometeia.com>
2021-04-19 14:52:04 +0200

    Bump version: 0.10.7 → 0.10.8

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fabiofumarola/polimorfo',
-    version='0.10.7',
+    version='0.10.8',
     zip_safe=False,
 )
