150fba7c610c07dfd88dfec0453901126c28ab4a
fabio fumarola <fabiofumarola@gmail.com>
2021-04-23 12:37:53 +0200

    Feature/visualize heatmaps (#252)
    * add font size to show_image and add method to save mask in multilabel mode
    
    * Bump version: 0.10.10 → 0.10.11
    
    Co-authored-by: Fabio Fumarola <fumarolaf@prometeia.com>

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fabiofumarola/polimorfo',
-    version='0.10.10',
+    version='0.10.11',
     zip_safe=False,
 )
