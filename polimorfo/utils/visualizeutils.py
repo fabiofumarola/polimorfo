8d2f4e0cb13edd9e0db39cb4c64ac86325cb0a2f
Fabio Fumarola <fumarolaf@prometeia.com>
2021-11-09 16:07:31 +0100

    update

--- polimorfo/utils/visualizeutils.py
+++ polimorfo/utils/visualizeutils.py
@@ -339,8 +339,6 @@ def draw_segmentation(
             conf_mask = np.array([1])
 
         bool_mask = probs[cat_idx] >= min_conf
-        conf_mask = probs[cat_idx][bool_mask]
-
         conf = np.round(np.nan_to_num(conf_mask.flatten().mean()), 2)
         if conf < min_conf:
             continue
